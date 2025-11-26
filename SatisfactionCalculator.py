5effab364abcd6473f0d676a0c34211c286aff5a
AllanDwyre <allan.dwyre@gmail.com>
2025-11-26 22:27:59 +0100

    SatisfactionCalculator updated formula

--- SatisfactionCalculator.py
+++ SatisfactionCalculator.py
@@ -1,39 +1,37 @@
 class SatisfactionCalculator:
-    def __init__(self,students_preferences,schools_preferences,matches):
-
+    def __init__(self, students_preferences, schools_preferences, matches):
         self.students_preferences = students_preferences
         self.schools_preferences = schools_preferences
-
         self.matches = matches
-
-        self.n=len(schools_preferences)
-
-        self.percentage_loss = 100//self.n 
-
-        self.students_satisfaction = [100]*self.n
-        self.schools_satisfaction = [100]*self.n
-
+        self.n = len(schools_preferences)
+        self.students_satisfaction = [0.0] * self.n
+        self.schools_satisfaction = [0.0] * self.n
         self.calculate_satisfaction()
     
     def calculate_satisfaction(self):
         for i in range(self.n):
-            self.students_satisfaction[i] -= self.getStudentGivenSchool(i)*self.percentage_loss
-            self.schools_satisfaction[i] -= self.getSchoolGivenStudent(i)*self.percentage_loss
+            student_rank = self.getStudentGivenSchool(i)
+            self.students_satisfaction[i] = round((1 - (student_rank / (self.n - 1))) * 100, 2)
+            
+            school_rank = self.getSchoolGivenStudent(i)
+            self.schools_satisfaction[i] = round((1 - (school_rank / (self.n - 1))) * 100, 2)
     
-    def getStudentGivenSchool(self,student_index):
-        return self.students_preferences[student_index].index(self.matches.index(student_index))
+    def getStudentGivenSchool(self, student_index):
+        school_obtained = self.matches.index(student_index)
+        return self.students_preferences[student_index].index(school_obtained)
     
-    def getSchoolGivenStudent(self,school_index):
-        return self.schools_preferences[school_index].index(self.matches[school_index])
-
+    def getSchoolGivenStudent(self, school_index):
+        student_obtained = self.matches[school_index]
+        return self.schools_preferences[school_index].index(student_obtained)
+    
     def getAverageStudentSatisfaction(self):
-        return sum(self.students_satisfaction)/self.n
+        return round(sum(self.students_satisfaction) / self.n, 2)
     
     def getAverageSchoolSatisfaction(self):
-        return sum(self.schools_satisfaction)/self.n
+        return round(sum(self.schools_satisfaction) / self.n, 2)
     
     def getGlobalSatisfaction(self):
-        return (self.getAverageStudentSatisfaction()+self.getAverageSchoolSatisfaction())/2
+        return round((self.getAverageStudentSatisfaction() + self.getAverageSchoolSatisfaction()) / 2, 2)
     
     def getStudentsSatisfaction(self):
         return self.students_satisfaction
